3dd6c416725bdc62919c2156451171f071a487c8
Christian <christian@twobulls.com>
2017-01-28 19:05:34 +1100

    Added C++ and CMake support

---
+++ LagrangeInterpolation/build/CMakeFiles/LagrangeInterpolation_Plugin.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/christian/Local/Math/LagrangeInterpolation/lagrange_interp_nd.cpp" "/Users/christian/Local/Math/LagrangeInterpolation/build/CMakeFiles/LagrangeInterpolation_Plugin.dir/lagrange_interp_nd.cpp.o"
+  "/Users/christian/Local/Math/LagrangeInterpolation/lagrange_interp_nd_prb.cpp" "/Users/christian/Local/Math/LagrangeInterpolation/build/CMakeFiles/LagrangeInterpolation_Plugin.dir/lagrange_interp_nd_prb.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ LagrangeInterpolation/build/CMakeFiles/LagrangeInterpolation_Plugin.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LagrangeInterpolation_Plugin.dir/lagrange_interp_nd.cpp.o"
+  "CMakeFiles/LagrangeInterpolation_Plugin.dir/lagrange_interp_nd_prb.cpp.o"
+  "libLagrangeInterpolation_Plugin.pdb"
+  "libLagrangeInterpolation_Plugin.dylib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LagrangeInterpolation_Plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
